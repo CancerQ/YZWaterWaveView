23c1db46c09623fb72bbda34f7ca3c5628f85eae
iOSRemove <443023263@qq.com>
2017-07-07 11:13:54 +0800

    Initial Commit

---
+++ YZWaterWaveView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZWaterWaveView
+//
+//  Created by 叶志强 on 2017/7/7.
+//  Copyright © 2017年 CancerQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZWaterWaveView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZWaterWaveView
+//
+//  Created by 叶志强 on 2017/7/7.
+//  Copyright © 2017年 CancerQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
